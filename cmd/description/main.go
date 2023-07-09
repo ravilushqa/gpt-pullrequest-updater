e650c550d1521bf0ed6bf9da32e5bbbbd1a7ab45
vanilevsky <vanilevsky@gmail.com>
2023-07-09 12:06:42 +0800

    Add ShortcutBaseURL param

--- cmd/description/main.go
+++ cmd/description/main.go
@@ -17,14 +17,15 @@ import (
 )
 
 var opts struct {
-	GithubToken string `long:"gh-token" env:"GITHUB_TOKEN" description:"GitHub token" required:"true"`
-	OpenAIToken string `long:"openai-token" env:"OPENAI_TOKEN" description:"OpenAI token" required:"true"`
-	Owner       string `long:"owner" env:"OWNER" description:"GitHub owner" required:"true"`
-	Repo        string `long:"repo" env:"REPO" description:"GitHub repo" required:"true"`
-	PRNumber    int    `long:"pr-number" env:"PR_NUMBER" description:"Pull request number" required:"true"`
-	OpenAIModel string `long:"openai-model" env:"OPENAI_MODEL" description:"OpenAI model" default:"gpt-3.5-turbo"`
-	Test        bool   `long:"test" env:"TEST" description:"Test mode"`
-	JiraURL     string `long:"jira-url" env:"JIRA_URL" description:"Jira URL. Example: https://jira.atlassian.com"`
+	GithubToken     string `long:"gh-token" env:"GITHUB_TOKEN" description:"GitHub token" required:"true"`
+	OpenAIToken     string `long:"openai-token" env:"OPENAI_TOKEN" description:"OpenAI token" required:"true"`
+	Owner           string `long:"owner" env:"OWNER" description:"GitHub owner" required:"true"`
+	Repo            string `long:"repo" env:"REPO" description:"GitHub repo" required:"true"`
+	PRNumber        int    `long:"pr-number" env:"PR_NUMBER" description:"Pull request number" required:"true"`
+	OpenAIModel     string `long:"openai-model" env:"OPENAI_MODEL" description:"OpenAI model" default:"gpt-3.5-turbo"`
+	Test            bool   `long:"test" env:"TEST" description:"Test mode"`
+	JiraURL         string `long:"jira-url" env:"JIRA_URL" description:"Jira URL. Example: https://jira.atlassian.com"`
+	ShortcutBaseURL string `long:"shortcut-url" env:"SHORTCUT_URL" description:"Shortcut URL. Example: https://app.shortcut.com/foo/"`
 }
 
 func main() {
